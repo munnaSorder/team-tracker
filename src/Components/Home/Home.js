88a63bf16fb3365f11c6cb82a9467511640a9e17
munnaSorder <developermunnaa@gmail.com>
2021-03-18 01:50:06 +0600

    fake data create done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1>working</h1>
+            <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit rem aperiam deleniti vero inventore, eos sed illo debitis impedit repellat accusantium, nam earum tempora accusamus cum facere vel mollitia et necessitatibus numquam explicabo! Eum nemo rerum, nulla laboriosam officiis possimus. Deleniti aperiam, quam suscipit impedit delectus quaerat obcaecati incidunt officia numquam laudantium in. Nesciunt qui reprehenderit quisquam molestias impedit dignissimos excepturi ducimus. Veniam amet blanditiis aspernatur obcaecati tenetur maiores et corrupti esse, fuga nobis! Explicabo saepe eos exercitationem maiores facere?</h1>
         </div>
     );
 };
